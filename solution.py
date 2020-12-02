e66a63f11a9352beefc813296855cc40bb7f00aa
Alexander Ignatov <yalishanda@abv.bg>
2020-12-02 11:58:34 +0200

    Create classifier stubs

--- solution.py
+++ solution.py
@@ -1,6 +1,16 @@
 """62136"""
 
+import pandas as pd
+
+class NaiveBayesClassifier:
+    def __init__(self, model: pd.DataFrame):
+        self.model = model
+
+    def predict(self, vector):
+        pass # TODO: return the most probable class
+
 if __name__ == "__main__":
-    import pandas as pd
     df = pd.read_csv("data/house-votes-84.data")
-    print(df)
+    df.columns = ["class"] + list(map(str, range(16)))
+    classifier = NaiveBayesClassifier(df)
+    assert "democrat" == classifier.predict(["n","y","y","n","y","y","n","n","n","n","n","n","y","y","y","y"]), "Underfitting"
